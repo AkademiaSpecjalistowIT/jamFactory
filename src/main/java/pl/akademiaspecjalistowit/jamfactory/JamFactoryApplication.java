db12b31205604dfab64dd4f6c0e359fc22d003ba
przemyslawgesieniecasit <przemyslaw.gesieniec@gmail.com>
2024-08-19 17:19:44 +0200

    init

---
+++ src/main/java/pl/akademiaspecjalistowit/jamfactory/JamFactoryApplication.java
@@ -0,0 +1,13 @@
+package pl.akademiaspecjalistowit.jamfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JamFactoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/akademiaspecjalistowit/jamfactory/JamFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akademiaspecjalistowit.jamfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
