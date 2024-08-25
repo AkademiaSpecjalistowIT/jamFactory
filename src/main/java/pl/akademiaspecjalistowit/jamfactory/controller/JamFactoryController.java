c6eb6f477be934b5f1bf21f2934f3e403de74b96
sginko <142085017+sginko@users.noreply.github.com>
2024-08-25 13:29:14 +0200

    add

--- src/main/java/pl/akademiaspecjalistowit/jamfactory/controller/JamFactoryController.java
+++ src/main/java/pl/akademiaspecjalistowit/jamfactory/controller/JamFactoryController.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.akademiaspecjalistowit.jamfactory.dto.JamPlanProduktionRequestDto;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/v1/jams")
 public class JamFactoryController {
@@ -18,6 +20,12 @@ public class JamFactoryController {
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/product-plan")
     public ResponseEntity<String> findProductionPlan(@RequestBody JamPlanProduktionRequestDto jamPlanProduktionRequestDto) {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body("NOT_IMPLEMENTED");
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/order/update/{id}")
+    public ResponseEntity<String> updateProductionPlan(@PathVariable("id") UUID id) {
         return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body("NOT_IMPLEMENTED");
     }
 }
